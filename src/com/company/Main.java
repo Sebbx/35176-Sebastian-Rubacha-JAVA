8add1373bad7950ca3bc293d0a1ef345529dc012
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-04-22 15:30:12 +0200

    7. Testowanie print, println oraz printf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
 public class Main {
-    public statiic void main(String[] args)
+    public static void main(String[] args)
     {
-        System.out.print("Hello Java world);");
+
+        System.out.print("Ptaki\n");
+        System.out.print("lataja\n");
+        System.out.print("kluczem\n");
+
+        int a = 5;
+        double b = 2.32;
+        String s = "Tekst";
+
+        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
+
+        System.out.print("Nazywaliśmy to „witaminą B3\"\n");
+
+        System.out.printf("alfa\tsin(alfa)\n");
+        for (int i = 0; i <370; i+=10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+
     }
 }
