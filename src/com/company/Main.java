9a3d0da4769d3f4013bc8f3e668d07214f0e5e34
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-04-22 17:45:20 +0200

    12. dodawanie kodu do własnej klasy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Account acc = new Account();
-        acc.setName ("Sebastian Rubacha");
+        acc.setName ("sebastian Rubacha");
         System.out.println(acc.getName());
+        System.out.printf("%s%n",Account.capitalize("stanisLaw maruSARz"));
     }
 
 }
--- src/com/company/mylib/Account.java
+++ src/com/company/mylib/Account.java
@@ -1,12 +1,30 @@
 package com.company.mylib;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Account {
-    public String name;
 
-    public void setName(String name)
+    private String name;
+
+    private static String ukrAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+    private static String translitRepl[] = {"a","b","v","g","d","e","je","zh","z","y","i","ji", "j",
+            "k","l","m","n","o","p","r","s","t","u","f","h","c",
+            "ch","sh","shh","'","ju","ja"};
+
+
+    public static String capitalize(String aStr)
     {
-        this.name = name;
+        String[] tokens = StringUtils.split(aStr);
+        for(int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        return StringUtils.join(tokens,' ');
     }
+    public void setName(String name) {
+        this.name = capitalize(name);
+    }
+
+
+
     public String getName()
     {
         return name;
