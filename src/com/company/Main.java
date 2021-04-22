377f747f8d46ee6dd5b0976e05d992de90e60855
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-04-22 16:57:55 +0200

    11.a/b) Dodana własna klasa Account / błąd kompilacji Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,13 @@
 package com.company;
 
+import com.company.mylib.Account;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    Account acc = new Account();
+    acc.setName("Sebastian Rubacha");
+    System.out.println(acc.getName());
 
-    public static String leftPad(String aText, char aChar, int aWidth)
-    {
-        String res = aText;
-        for (int i = 0; i < aWidth * aText.length(); i++)
-            res = aChar + res;
-        return res;
-    }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num1;
-        System.out.print("Podaj liczbę do przekonwertowania na typ szesnastkowy ");
-        num1 = input.nextInt();
-        String hexNum = leftPad(Integer.toHexString(num1), '0', 3);
-        String binNum = leftPad(Integer.toBinaryString(num1), '0', 1);
-        System.out.println("liczba " + num1 + " w:\nPostaci szesnastkowej to " + hexNum.toUpperCase() + "\nPostaci binarnej to " + binNum.toUpperCase() );
-    }
 }
