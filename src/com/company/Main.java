dac5e9a7a5da3140ec9e9e651cc923aebfc47dd2
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-04-22 15:38:16 +0200

    8. Użycie obiektu Scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-
-        System.out.print("Ptaki\n");
-        System.out.print("lataja\n");
-        System.out.print("kluczem\n");
-
-        int a = 5;
-        double b = 2.32;
-        String s = "Tekst";
-
-        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
-
-        System.out.print("Nazywaliśmy to „witaminą B3\"\n");
-
-        System.out.printf("alfa\tsin(alfa)\n");
-        for (int i = 0; i <370; i+=10)
-            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        Scanner input = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("Podaj pierwszą liczbę: ");
+        num1 = input.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        num2 = input.nextInt();
+        System.out.printf("Wynik dodawania %d + %d = %d%n", num1,num2,num1+num2);
 
     }
 }
